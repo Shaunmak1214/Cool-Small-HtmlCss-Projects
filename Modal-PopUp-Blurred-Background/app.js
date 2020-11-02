65ecb0a689e1ad2a9ecc58ea086a97a0fb46067d
Shaunmak1214 <shaonmak@gmail.com>
2020-11-02 14:37:10 +0800

    Added Modal PopUp

---
+++ Modal-PopUp-Blurred-Background/app.js
@@ -0,0 +1,9 @@
+function toggle() {
+
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+
+    var popup = document.getElementById('popup');
+    popup.classList.toggle('active');
+
+}   
